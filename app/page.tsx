a8a912951b7d41079b9eecc15debb9dad89fbf6e
Karun Agarwal <singhalkarun@gmail.com>
2025-08-10 23:29:23 +0530

    landing page initial: don't judge if you see this commit

--- app/page.tsx
+++ app/page.tsx
@@ -2,102 +2,47 @@ import Image from "next/image";
 
 export default function Home() {
   return (
-    <div className="font-sans grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20">
-      <main className="flex flex-col gap-[32px] row-start-2 items-center sm:items-start">
-        <Image
-          className="dark:invert"
-          src="/next.svg"
-          alt="Next.js logo"
-          width={180}
-          height={38}
-          priority
-        />
-        <ol className="font-mono list-inside list-decimal text-sm/6 text-center sm:text-left">
-          <li className="mb-2 tracking-[-.01em]">
-            Get started by editing{" "}
-            <code className="bg-black/[.05] dark:bg-white/[.06] font-mono font-semibold px-1 py-0.5 rounded">
-              app/page.tsx
-            </code>
-            .
+    <div className="p-16">
+      <div className="py-2">
+        <h1 className="text-4xl font-bold">Karun Agarwal</h1>
+        <p className="text-lg">Software Engineer / Pet Parent</p>
+      </div>
+      <div className="py-2">
+        <h2 className="text-2xl font-bold">About Me</h2>
+        <p>
+          A technology enthusiast who skipped college to pursue his passion for solving real-world complex problems using tech, currently building in voice ai/ synthetic humans space
+        </p>
+      </div>
+      <div className="py-2">
+        <h2 className="text-2xl font-bold">Corporate Experience </h2>
+        <ul className="pt-1">
+          <li>
+            <p className="font-bold"><a href="https://www.olakrutrim.com" target="_blank" className="underline text-blue-500">Krutrim</a> - Principal Engineer - Feb 2025 - Present</p>
           </li>
-          <li className="tracking-[-.01em]">
-            Save and see your changes instantly.
+          <li>
+            <p className="font-bold"><a href="https://samagragovernance.in" target="_blank" className="underline text-blue-500">Samagra</a> - Senior Software Engineer - Sep 2023 - Feb 2025</p>
           </li>
-        </ol>
-
-        <div className="flex gap-4 items-center flex-col sm:flex-row">
-          <a
-            className="rounded-full border border-solid border-transparent transition-colors flex items-center justify-center bg-foreground text-background gap-2 hover:bg-[#383838] dark:hover:bg-[#ccc] font-medium text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5 sm:w-auto"
-            href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            <Image
-              className="dark:invert"
-              src="/vercel.svg"
-              alt="Vercel logomark"
-              width={20}
-              height={20}
-            />
-            Deploy now
-          </a>
-          <a
-            className="rounded-full border border-solid border-black/[.08] dark:border-white/[.145] transition-colors flex items-center justify-center hover:bg-[#f2f2f2] dark:hover:bg-[#1a1a1a] hover:border-transparent font-medium text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5 w-full sm:w-auto md:w-[158px]"
-            href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Read our docs
-          </a>
-        </div>
-      </main>
-      <footer className="row-start-3 flex gap-[24px] flex-wrap items-center justify-center">
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/file.svg"
-            alt="File icon"
-            width={16}
-            height={16}
-          />
-          Learn
-        </a>
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/window.svg"
-            alt="Window icon"
-            width={16}
-            height={16}
-          />
-          Examples
-        </a>
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://nextjs.org?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/globe.svg"
-            alt="Globe icon"
-            width={16}
-            height={16}
-          />
-          Go to nextjs.org →
-        </a>
-      </footer>
+          <li>
+            <p className="font-bold"><a href="https://esmagico.com" target="_blank" className="underline text-blue-500">EsMagico</a> - Software Engineer 2 - Jul 2022 - Aug 2023</p>
+          </li>
+          <li>
+            <p className="font-bold">Revolute - Founding Engineer - Jan 2022 - Jun 2022</p>
+          </li>
+          <li>
+            <p className="font-bold"><a href="https://www.oodles.com/" target="_blank" className="underline text-blue-500">Oodles</a> - Associate Consultant - Feb 2021 - Dec 2021</p>
+          </li>
+          <li>
+            <p className="font-bold">FutureX - Founding Engineer - May 2020 - Jan 2021</p>
+          </li>
+          <li>
+            <p className="font-bold">Chatpod - Founding Engineer - Dec 2019 - Nov 2020</p>
+          </li>
+          <li>
+            <p className="font-bold"><a href="https://www.linkedin.com/company/toppr-com" target="_blank" className="underline text-blue-500">Toppr</a> - Content Developer - Jan 2019 - Dec 2019</p>
+          </li>
+        </ul>
+      </div>
     </div>
+
   );
 }
